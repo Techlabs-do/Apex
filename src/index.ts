21d005b510f837dbd28c3f5daa1844cc1d7cfb67
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-25 20:24:08 +0500

    added middleware

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { HttpException, NotFoundException, UnAuthorizedException, ValidationException } from './errors'
+export { IErrorInterface } from './interfaces/error.interface'
+export { Request, User } from './interfaces/request.interface'
+export { default as withHandler } from './middleware/withHandler'
+export { withParamValidation } from './middleware/withParamValidation'
+export { withValidation } from './middleware/withValidation'
 export { COMPANY_NOT_FOUND, BODY_NOT_FOUND, PRODUCT_NOT_FOUND, NOT_FOUND, VALIDATION_FAILED, UNAUTHORIZED } from './utils/enums/error.enum'
