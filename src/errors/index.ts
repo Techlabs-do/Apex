236cddbc93e6f1fbaaec80ea4add29454db65391
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-07-25 14:49:29 +0500

    add index

---
+++ src/errors/index.ts
@@ -0,0 +1,4 @@
+import { HttpException } from './HttpException'
+import { NotFoundException } from './NotFoundException'
+import { UnAuthorizedException } from './UnAuthorizedException'
+import { ValidationException } from './ValidationException'
