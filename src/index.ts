f994910d3609622e9c835a187fa3c493266b36b8
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-08-01 18:54:04 +0500

    fix: set version

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { default as withHandler } from './middleware/withHandler'
 export { withParamValidation } from './middleware/withParamValidation'
 export { withValidation } from './middleware/withValidation'
 export { COMPANY_NOT_FOUND, BODY_NOT_FOUND, PRODUCT_NOT_FOUND, NOT_FOUND, VALIDATION_FAILED, UNAUTHORIZED } from './utils/enums/error.enum'
+
